cb58361251f2218bb0cb3e0f788f437f157cfd65
Kevin Agudo Montil <121159576+Kevinky107@users.noreply.github.com>
2024-09-02 12:01:28 +0200

    Feat: index 1 code

---
+++ index1.php
@@ -0,0 +1,12 @@
+<? 
+    $rooms = [
+        ['ID' => 0, 'Name' => 'Suite', 'Number' => 107, 'Price' => 350, 'Discount' => 300],
+        ['ID' => 1, 'Name' => 'Double Bed', 'Number' => 108, 'Price' => 250, 'Discount' => 220],
+        ['ID' => 2, 'Name' => 'Single Bed', 'Number' => 109, 'Price' => 200, 'Discount' => 190]
+    ];
+?> 
+
+<h1>Rooms</h1>
+<pre>
+    <?php print_r($rooms) ?>
+</pre>

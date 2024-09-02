328f6434c4984ac21feaab291e4d773d69af6170
Kevin Agudo Montil <121159576+Kevinky107@users.noreply.github.com>
2024-09-02 19:14:26 +0200

    Update index5.php

--- index5.php
+++ index5.php
@@ -1,26 +1,31 @@
 <?php 
-    $mysqli = mysqli_connect("localhost","root","kev121514","hotelmiranda");
-    if (mysqli_connect_errno()) {
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    $mysqli = new mysqli("localhost", "root", "Kev!121514", "hotelmiranda");
+
+    if ($mysqli->connect_errno) {
+        echo "Error al conectar con MySQL: " . $mysqli->connect_error;
         exit();
     }
-      
-    $sql = "SELECT * FROM Rooms";
-    $rooms = mysqli_query($con, $sql);
-      
-    mysqli_free_result($rooms);
-      
-    mysqli_close($con);
+
+    $sql = "SELECT * FROM rooms";
+    $rooms = $mysqli->query($sql);
+
+    if (!$rooms) {
+        echo "Error en la consulta: " . $mysqli->error;
+        exit();
+    }
 ?> 
 
 <h1>Rooms</h1>
 
-
 <?php foreach ($rooms as $room): ?>
-    <ol>
-        <li>Name: <?= $room['name'] ?></li>
-        <li>Number: <?= $room['id'] ?></li>
-        <li>Price: <?= $room['price'] ?></li>
-        <li>Discount: <?= round(100-($room['offer'] * 100)/$room['price']) ?>%</li>
-    </ol>
+    <ul>
+        <li>Name: <?= htmlspecialchars($room['name']) ?></li>
+        <li>Number: <?= htmlspecialchars($room['_id']) ?></li>
+        <li>Price: <?= htmlspecialchars($room['price']) ?></li>
+        <li>Discount: <?= round(100 - ($room['offer'] * 100) / $room['price']) ?>%</li>
+    </ul>
 <?php endforeach; ?>
+
+<?php
+    $mysqli->close();
+?>

906cea9ad3d9f0b5b6f80753e7945ffd05ff57c0
Kevin Agudo Montil <121159576+Kevinky107@users.noreply.github.com>
2024-09-02 14:56:25 +0200

    Update index5.php

--- index5.php
+++ index5.php
@@ -1,5 +1,5 @@
 <?php 
-    $mysqli = new mysqli("localhost","root","kev121514","hotelmiranda");
+    $mysqli = mysqli_connect("localhost","root","kev121514","hotelmiranda");
     if (mysqli_connect_errno()) {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
         exit();

d4e29d2cf063ca6ad9dcb0749fbbbe402d1547d2
Kevin Agudo Montil <121159576+Kevinky107@users.noreply.github.com>
2024-09-02 13:20:34 +0200

    Update index5.php

---
+++ index5.php
@@ -0,0 +1,26 @@
+<?php 
+    $mysqli = new mysqli("localhost","root","kev121514","hotelmiranda");
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        exit();
+    }
+      
+    $sql = "SELECT * FROM Rooms";
+    $rooms = mysqli_query($con, $sql);
+      
+    mysqli_free_result($rooms);
+      
+    mysqli_close($con);
+?> 
+
+<h1>Rooms</h1>
+
+
+<?php foreach ($rooms as $room): ?>
+    <ol>
+        <li>Name: <?= $room['name'] ?></li>
+        <li>Number: <?= $room['id'] ?></li>
+        <li>Price: <?= $room['price'] ?></li>
+        <li>Discount: <?= round(100-($room['offer'] * 100)/$room['price']) ?>%</li>
+    </ol>
+<?php endforeach; ?>
